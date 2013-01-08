045dd6d92911e003e3b24ab3c22cd4e2e20d2c72
smrahmani <s.mostafa.rahmani@gmail.com>
2013-01-08 08:44:00 -0800

    Create Files.php

---
+++ Files.php
@@ -0,0 +1,30 @@
+<?php
+
+class Files {
+
+    public static $myVar = 'some value';
+
+    public static function DoSomething() {
+        echo self::$myVar;
+    }
+
+    function __construct() {
+        
+    }
+
+    private static $timesFileIsChecked = 0;
+
+    public static function CheckImage($file) {
+        self::$timesFileIsChecked++;
+        $size = getimagesize($file);
+        //echo '<pre>'; print_r($size); echo '</pre>';
+        return (!empty($size) ) ? $size : 'Not an image';
+    }
+
+    public static function GetNumChecked() {
+        return self::$timesFileIsChecked;
+    }
+
+}
+
+?>
